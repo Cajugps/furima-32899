27a6a811dd315bc2a18dc704088ca7b1df88758e
Cajugps <bpskge@gmail.com>
2021-04-27 20:49:16 +0900

    emailのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  validates :email,presence: true,uniqueness: true
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
@@ -13,4 +14,5 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6,12}\z/
   validates :password, presence: true,
                        format: { with: VALID_PASSWORD_REGEX }
+  
 end
