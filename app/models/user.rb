4a4a8b188f60f8c90d889e6fe95ec5b259eebd92
Cajugps <bpskge@gmail.com>
2021-04-30 21:50:24 +0900

    バリデーション編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :email,presence: true,uniqueness: true
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6,12}\z/
-  validates :password, presence: true,
-                       format: { with: VALID_PASSWORD_REGEX }
-  
+         
+       with_options presence: true do
+       validates :nickname
+       with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+       validates :last_name
+       validates :first_name
+       end 
+       with_options format: {with: /\A[ァ-ヶ]+\z/} do 
+       validates :last_name_kana
+       validates :first_name_kana
+       end 
+       end 
+       validates :birthday,presence: true
+       VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+       validates :password,presence:true,format: { with: VALID_PASSWORD_REGEX }
+
 end
